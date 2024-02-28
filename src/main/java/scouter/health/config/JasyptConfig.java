81455d5e4b8b2529952c0c015a9d7139c81f5860
lkhlkh23 <lkhlkh09@gmail.com>
2024-02-28 17:20:37 +0900

    db 설정

---
+++ src/main/java/scouter/health/config/JasyptConfig.java
@@ -0,0 +1,22 @@
+package scouter.health.config;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JasyptConfig {
+
+	@Value("${jasypt.encryptor.password}")
+	private String encryptKey;
+
+	@Bean
+	public StandardPBEStringEncryptor standardPBEStringEncryptor() {
+		final StandardPBEStringEncryptor standardPBEStringEncryptor = new StandardPBEStringEncryptor();
+		standardPBEStringEncryptor.setAlgorithm("PBEWithMD5AndDES");
+		standardPBEStringEncryptor.setPassword(encryptKey);
+		return standardPBEStringEncryptor;
+	}
+
+}
---
+++ src/test/java/scouter/health/config/JasyptConfigTest.java
@@ -0,0 +1,28 @@
+package scouter.health.config;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JasyptConfigTest {
+
+	@Autowired
+	private StandardPBEStringEncryptor sut;
+
+	@Test
+	void test_encrypt() {
+		// given
+		final String text = "password";
+
+		// when
+		final String result = sut.encrypt(text);
+
+		// then
+		System.out.println(result + "~~~~~~~~");
+		assertNotNull(result);
+	}
+}
