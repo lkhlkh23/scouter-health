89fb482fdc50bc689e35e18df3916ef773b40052
lkhlkh23 <lkhlkh09@gmail.com>
2024-02-28 16:37:09 +0900

    프로젝트 초기 생성

---
+++ src/main/java/scouter/health/ScouterHealthApplication.java
@@ -0,0 +1,13 @@
+package scouter.health;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScouterHealthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScouterHealthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/scouter/health/ScouterHealthApplicationTests.java
@@ -0,0 +1,13 @@
+package scouter.health;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScouterHealthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
